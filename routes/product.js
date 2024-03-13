16e49a2bfa0c99f1547c8cfe3c2c5ed6a7ae4773
unknown <adelformat541@gmail.com>
2024-03-13 18:40:50 +0100

    update

--- routes/product.js
+++ routes/product.js
@@ -4,7 +4,7 @@ module.exports = (express) => {
     const router = express.Router();
  router.post("/createProduct",upload.single('image'), productController.createProduct);
     router.get("/", productController.getProducts);
-    router.put("/:id", productController.updateOneProduct);
+    router.put("/:id", upload.single('image'), productController.updateOneProduct);
     router.delete("/:id", productController.deleteProduct);
     router.get("/:id", productController.getProductWithId);
     return router;
